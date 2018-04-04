3ad5576e7738cedc081f05be60d45232b8ba14ca
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-04 12:33:55 -0600

    finished enroll in summer school

---
+++ enrollInSummerSchool.js
@@ -0,0 +1,26 @@
+'use strict';
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+  students.forEach(item => {
+    item.status = 'In Summer school';
+  });
+  return students;
+}
+console.log(enrollInSummerSchool(studentData));
